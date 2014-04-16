5613f5192c4682e1f09a6dae0d74def2d9f35b13
turbo100500 <superllout@mail.ru>
2014-04-16 13:48:50 +0000

    auth problem

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Pro100::Application.config.secret_token = '97e69d4515ecb216e8bea65fcad58e19766f703c976cb04a9e410ce4940ed06921322b0f601f19766d471a97da583ea7e0fe67e7091fd0fdb0d9981527541afa'
